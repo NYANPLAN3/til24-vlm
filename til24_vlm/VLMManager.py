8a0a6fbc62fdfa9b3fe254f76d191bf0b026ee94
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-05-24 13:16:00 +0000

    feat: use cache to speed up inference

--- til24_vlm/VLMManager.py
+++ til24_vlm/VLMManager.py
@@ -3,9 +3,11 @@
 import io
 from typing import List
 
+import numpy as np
 import open_clip
 import torch
-from open_clip import pretrained
+import torch.nn.functional as F
+import xxhash
 from PIL import Image
 from ultralytics import YOLO
 
@@ -16,7 +18,7 @@ CLIP_PATH = (
 
 YOLO_OPTS = dict(
     conf=0.05,
-    iou=0.2,
+    iou=0.0,
     imgsz=1536,
     half=True,
     device="cuda",
@@ -42,37 +44,48 @@ class VLMManager:
         )
         self.model.cuda().eval()
         self.tokenizer = open_clip.get_tokenizer("ViT-H-14-quickgelu")
+        self.hasher = xxhash.xxh64_hexdigest
+        self._cache = dict()
+
+    def _calc_im(self, im: Image.Image):
+        # Get bboxes using YOLO.
+        results = self.yolo.predict(im, **YOLO_OPTS)
+        bboxes = results[0].boxes.xyxy.tolist()
+        tens = []
+        for l, t, r, b in bboxes:
+            crop = im.crop((l, t, r, b))
+            tens.append(self.preprocess(crop))
+
+        # NOTE: We purposefully return invalid input if not found; That way, the eval system leaks how many failed altogether.
+        if len(tens) == 0:
+            return None
+
+        # Normalize & cache crop embeddings.
+        bat = torch.stack(tens).to("cuda")
+        out = F.normalize(self.model.encode_image(bat))
+        embs: np.ndarray = out.numpy(force=True)
+        return bboxes, embs.T
+
+    def _calc_txt(self, caption):
+        tens = self.tokenizer(caption).to("cuda")
+        out = F.normalize(self.model.encode_text(tens))
+        emb: np.ndarray = out.numpy(force=True)
+        return emb
 
     @torch.inference_mode()
     @torch.autocast("cuda")
     def identify(self, image: bytes, caption: str) -> List[int]:
         """Identify."""
-        # TODO: The eval code wastefully repeats images for each eval call, within a batch, might be able to cache image?
-        file = io.BytesIO(image)
-        im = Image.open(file)
+        imhash = self.hasher(image)
+        if imhash in self._cache:
+            bboxes, crop_embs = self._cache[imhash]
+        else:
+            file = io.BytesIO(image)
+            im = Image.open(file)
+            self._cache[imhash] = bboxes, crop_embs = self._calc_im(im)
 
-        # Get bboxes using YOLO.
-        results = self.yolo.predict(im, **YOLO_OPTS)
-        bboxes = results[0].boxes.xyxy.tolist()
-        crops = [im.crop((l, t, r, b)) for l, t, r, b in bboxes]
-        crop_tens = []
-        for crop in crops:
-            crop_tens.append(self.preprocess(crop))
-
-        # NOTE: We purposefully return invalid input if not found; That way, the eval system leaks how many failed altogether.
-        if len(crop_tens) == 0:
-            return None
-
-        crop_batch = torch.stack(crop_tens).to("cuda")
-        caption_tens = self.tokenizer(caption).to("cuda")
-
-        crop_embs = self.model.encode_image(crop_batch)
-        caption_emb = self.model.encode_text(caption_tens)
-
-        crop_embs /= crop_embs.norm(dim=-1, keepdim=True)
-        caption_emb /= caption_emb.norm(dim=-1, keepdim=True)
-
-        crop_probs = (100.0 * caption_emb @ crop_embs.T).softmax(dim=-1)
+        caption_emb = self._calc_txt(caption)
+        crop_probs = caption_emb @ crop_embs
         idx = crop_probs.argmax().item()
 
         x1, y1, x2, y2 = bboxes[idx]
